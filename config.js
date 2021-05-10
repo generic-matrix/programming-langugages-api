5501c7f97fa9826fbe6ee7f0fa3eb761b502e45c
kaustubhb <kaustubhbawa123@gmail.com>
2021-05-10 14:04:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const env = process.env;
 const config = {
   db: { /* don’t expose password or any sensitive info, done only for demo */
     host: env.DB_HOST || '10.3.0.2',
-    user: env.DB_USER || 'user1',
-    password: env.DB_PASSWORD || 'user1',
+    user: env.DB_USER || 'root',
+    password: env.DB_PASSWORD || 'root',
     database: env.DB_NAME || 'data',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
