2125b5028a9491bd583867320ed847f12ad5e34a
kaustubhb <kaustubhbawa123@gmail.com>
2021-05-13 13:55:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.json({'message': 'ok changed default'});
+  res.json({'message': 'ok changed now '});
 })
 
 app.use('/programming-languages', programmingLanguagesRouter);
